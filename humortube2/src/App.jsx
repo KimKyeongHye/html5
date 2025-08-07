93c152d404fc775b96509da0982de1754d0431e3
KimKyeongHye <kkh0908@lx.or.kr>
2025-08-07 16:38:56 +0900

    유머튜브 완성

---
+++ humortube2/src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+
+  return (
+    <>
+    <div className="center_div">
+      <h1>ISA계좌 알아가기</h1>
+      <h2>안 만들면 손해 ISA</h2>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/8WCqmhHftl8?si=llP7_TXGl4qFg7pT&amp;start=4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <h2>ISA할까? 연금저출할까?</h2>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/574Oi3Jn_KQ?si=IzynWRf9LeMhf5qp&amp;start=3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <h2>ISA계좌 배당주 사는법</h2>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/i9fXr3U-ReA?si=WPwg4i9ECOp_21kE&amp;start=3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+    <h1>연금저축 알아보기</h1>
+      <h2>연금저축 사용설명서</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/sx5blWMu0J4?si=5qxlcEGJprtKWHrb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      <h2>55세 연금준비?</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/mZBOJaGGz-I?si=UbrcT-Y5MtI0uMLa" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+    </>
+  )
+}
+
+export default App
